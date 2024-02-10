18ba2743a859a3661be51c23cb170722b6008bc2
yuki <yukiobata1@gmail.com>
2024-02-10 16:03:36 +0900

    changed nextconfig.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
+// next.config.js
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, options) => {
+    // Add a custom path alias
+    config.resolve.alias['@'] = path.resolve(__dirname, '.');
+
+    // Example: Add an additional rule or modify the config further
+    // config.module.rules.push({
+    //   test: /\.svg$/,
+    //   use: ["@svgr/webpack"]
+    // });
+
+    // Return the modified config
+    return config;
+  },
+};
 
 module.exports = nextConfig;
